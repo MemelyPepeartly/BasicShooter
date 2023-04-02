15f79c6933c6ba630298fbd7419beb027e0e9840
MemelyPepeartly <memelypepeartly@gmail.com>
2023-04-01 23:03:31 -0600

    movement

--- src/objects/Player.js
+++ src/objects/Player.js
@@ -1,6 +1,12 @@
-export default class Player extends Sprite{
-    constructor(x , y)
+import Sprite from "./Sprite.js"
+
+/**
+ * Player object
+ */
+export default class Player extends Sprite {
+    constructor(x, y)
     {
+        super();
         this.x = x;
         this.y = y;
 
@@ -8,11 +14,77 @@ export default class Player extends Sprite{
         this.height = 50;
 
         this.speed = 4;
+
+        document.addEventListener("keydown", this.keydown);
+        document.addEventListener("keyup", this.keyup);
     }
-    draw() {
+    draw(ctx) {
+        this.move();
         ctx.strokeStyle = "white";
-        ctx.fillStyle = "black"
         ctx.strokeRect(this.x, this.y, this.width, this.height);
+        
+        ctx.fillStyle = "black"
         ctx.fillRect(this.x, this.y, this.width, this.height);
+
+        this.shoot();
+    }
+
+    move() {
+        if(this.downPressed) {
+            this.y += this.speed;
+        }
+        if(this.upPressed) {
+            this.y -= this.speed;
+        }
+        if(this.leftPressed) {
+            this.x -= this.speed;
+        }
+        if(this.rightPressed) {
+            this.x += this.speed;
+        }
+    }
+
+    shoot() {
+        if(this.shootPressed) {
+            console.log("shoot");
+        }
+    }
+
+    keydown = (event) => {
+        if(event.code === "KeyW") {
+            this.upPressed = true;
+        }
+        if(event.code === "KeyS") {
+            this.downPressed = true;
+        }
+        if(event.code === "KeyA") {
+            this.leftPressed = true;
+        }
+        if(event.code === "KeyD") {
+            this.rightPressed = true;
+        }
+
+        if(event.code === "Space") {
+            this.shootPressed = true;
+        }
+    }
+
+    keyup = (event) => {
+        if(event.code === "KeyW") {
+            this.upPressed = false;
+        }
+        if(event.code === "KeyS") {
+            this.downPressed = false;
+        }
+        if(event.code === "KeyA") {
+            this.leftPressed = false;
+        }
+        if(event.code === "KeyD") {
+            this.rightPressed = false;
+        }
+
+        if(event.code === "Space") {
+            this.shootPressed = false;
+        }
     }
 }
