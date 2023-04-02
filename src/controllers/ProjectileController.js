4c08c480787251f1af007a6c639bf85b2498df02
MemelyPepeartly <memelypepeartly@gmail.com>
2023-04-01 23:30:29 -0600

    new controller functionality

--- src/controllers/ProjectileController.js
+++ src/controllers/ProjectileController.js
@@ -1,6 +1,35 @@
-export default class ProjectileController {
-    contructor()
-    {
+import Projectile from "../objects/Projectile.js";
 
+export default class ProjectileController {
+    projectiles = [];
+    timerTillNextProjectile = 0;
+    contructor(canvas)
+    {
+        this.canvas = canvas;
+    }
+
+    draw(ctx) {
+
+        this.projectiles.forEach((projectile) => {
+            if(this.isProjectileOffScreen(projectile))
+            {
+                const index = this.projectiles.indexOf(projectile);
+                this.projectiles.splice(index, 1);
+            }
+            projectile.draw(ctx);
+        })
+    }
+    shoot(x, y, speed, damage, delay) {
+        if(this.timerTillNextProjectile <= 0)
+        {
+            this.projectiles.push(new Projectile(x, y, speed, damage));
+            this.timerTillNextProjectile = delay;
+        }
+        this.timerTillNextProjectile--;
+    }
+
+    isProjectileOffScreen(projectile)
+    {
+        return projectile.y <= -projectile.height;
     }
 }
