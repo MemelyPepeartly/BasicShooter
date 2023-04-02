a8d6a4d96109fab27dd8b31a8035fbbb5cd80578
MemelyPepeartly <memelypepeartly@gmail.com>
2023-04-01 22:38:57 -0600

    default sprite for extends

---
+++ src/objects/Sprite.js
@@ -0,0 +1,14 @@
+export default class Sprite{
+    constructor(x, y)
+    {
+        this.x = x;
+        this.y = y;
+
+        this.width = 50;
+        this.height = 50;
+
+        this.speed = 4;
+    }
+    
+    draw(ctx) {}
+}
