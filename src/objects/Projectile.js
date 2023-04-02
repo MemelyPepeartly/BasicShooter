38fccf9df83396451a3f00e52870907f3850c334
MemelyPepeartly <memelypepeartly@gmail.com>
2023-04-01 23:30:59 -0600

    projectile construction

--- src/objects/Projectile.js
+++ src/objects/Projectile.js
@@ -4,5 +4,22 @@ import Sprite from "./Sprite.js"
  * Projectile object
  */
 export default class Projectile extends Sprite {
+    constructor(x, y, speed, damage) {
+        super();
 
+        this.x = x,
+        this.y = y
+        this.speed = speed;
+        this.damage = damage;
+
+        this.width = 5;
+        this.height = 15;
+        this.color = "red";
+    }
+    draw(ctx) {
+        ctx.fillStyle = this.color;
+        this.y -= this.speed;
+
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
 }
